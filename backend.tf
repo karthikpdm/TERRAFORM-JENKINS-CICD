d6dad818b09cd9ea2b587a4f0f67fec236dda1fc
karthikbm <108477836+karthikpdm@users.noreply.github.com>
2024-02-29 21:33:04 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terrabackend1"
-    key            = "backend1/"
+    key            = "backend1"
     region         = "us-east-2"
     dynamodb_table = "mydynamo"
   }
